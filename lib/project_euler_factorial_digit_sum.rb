f22a690715baace9e144871bd01b0b7829d18f32
Kunal <kdshah6593@gmail.com>
2021-08-24 14:52:27 -0400

    completed solution, all tests passing

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,13 @@
 # code your solution here
+def factorial(num)
+    product = (1..num).reduce{|product, n| product * n}
+end
+
+def sum_of_digits(num)
+    sum = num.digits.reduce{|sum, n| sum + n}
+end
+
+
+def factorial_digit_sum(num)
+    sum_of_digits(factorial(num))
+end
